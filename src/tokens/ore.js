4e872573a9042a1489f8034157ac1a7d86c38d95
Surabhi Lodha <surabhil@usc.edu>
2019-03-12 17:39:23 -0700

    ORE no longer has approve functionality

--- src/tokens/ore.js
+++ src/tokens/ore.js
@@ -9,15 +9,6 @@ function issueOre(toAccountName, oreAmount, memo = '') {
   return this.issueToken(toAccountName, amount, ORE_ORE_ACCOUNT_NAME, CONTRACT_NAME, memo);
 }
 
-async function approveOre(fromAccountName, toAccountName, oreAmount, memo = '', permission = 'active') {
-  amount = this.getAmount(oreAmount, TOKEN_SYMBOL);
-  const fromAccountBalance = await this.getOreBalance(fromAccountName, TOKEN_SYMBOL, CONTRACT_NAME);
-  if (fromAccountBalance > 0) {
-    return this.approveTransfer(fromAccountName, toAccountName, amount, CONTRACT_NAME, memo, permission);
-  }
-  throw new Error('The account does not have sufficient balance');
-}
-
 function getOreBalance(oreAccountName) {
   return this.getBalance(oreAccountName, TOKEN_SYMBOL, CONTRACT_NAME);
 }
@@ -27,15 +18,8 @@ function transferOre(fromAccountName, toAccountName, oreAmount, memo = '') {
   return this.transferToken(fromAccountName, toAccountName, amount, CONTRACT_NAME, memo);
 }
 
-function transferOrefrom(approvedAccountName, fromAccountName, toAccountName, oreAmount, memo) {
-  amount = this.getAmount(oreAmount, TOKEN_SYMBOL);
-  return this.transferFrom(approvedAccountName, fromAccountName, toAccountName, amount, CONTRACT_NAME, memo);
-}
-
 module.exports = {
   issueOre,
-  approveOre,
   getOreBalance,
   transferOre,
-  transferOrefrom,
 };
--- test/tokens/ore.test.js
+++ test/tokens/ore.test.js
@@ -29,43 +29,6 @@ describe('ore', () => {
     });
   });
 
-  describe('approveOre', () => {
-    let oreBalance;
-    let memo;
-    let spyTransaction;
-    let transaction;
-
-    beforeEach(() => {
-      oreBalance = 10;
-      memo = 'approve ORE transfer';
-      fetch.resetMocks();
-      fetch.mockResponses(mock([`${oreBalance}.0000 ORE`]));
-      orejs = constructOrejs({ fetch });
-      transaction = mockGetTransaction(orejs);
-      spyTransaction = jest.spyOn(orejs.eos, 'transact');
-    });
-
-    describe('when authorized', () => {
-      it('returns', async () => {
-        mockGetInfo(orejs);
-        mockGetBlock(orejs);
-        const result = await orejs.approveOre(ORE_OWNER_ACCOUNT_NAME, ORE_TESTA_ACCOUNT_NAME, oreBalance, memo);
-        expect(spyTransaction).toHaveBeenCalledWith({ actions: [mockAction({ account: 'eosio.token', name: 'approve' })] }, mockOptions());
-      });
-    });
-
-    describe('when unauthorized', () => {
-      xit('throws', () => {
-        mockGetInfo(orejs);
-        mockGetBlock(orejs);
-        contract.approve.mockImplementationOnce(() => Promise.reject(new Error('unauthorized')));
-
-        const result = orejs.approveOre(ORE_TESTA_ACCOUNT_NAME, ORE_TESTA_ACCOUNT_NAME, oreBalance);
-        expect(result).rejects.toThrow(/unauthorized/);
-      });
-    });
-  });
-
   describe('transferOre', () => {
     let oreBalance;
     let spyTransaction;
