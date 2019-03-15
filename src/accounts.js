1548ed8ccf01efc2a301e95133b7a21c24c9b9b9
Tyler Kuhn <tyler@wargoats.com>
2019-03-15 13:04:53 -0700

    do not re-encrypt already encrypted keys

--- src/accounts.js
+++ src/accounts.js
@@ -94,12 +94,19 @@ function generateAccountNameString(prefix = '') {
   return (prefix + timestampEosBase32() + randomEosBase32()).substr(0, 12);
 }
 
+function encrypted(key) {
+  if (key.match(/^\{.*\}$/)) {
+    return true;
+  }
+  return false;
+}
+
 function encryptKeys(keys, password, salt) {
   const { privateKeys, publicKeys } = keys;
   const encryptedKeys = {
     privateKeys: {
-      owner: this.encrypt(keys.privateKeys.owner, password, salt).toString(),
-      active: this.encrypt(keys.privateKeys.active, password, salt).toString()
+      owner: encrypted(keys.privateKeys.owner) ? keys.privateKeys.owner : this.encrypt(keys.privateKeys.owner, password, salt).toString(),
+      active: encrypted(keys.privateKeys.active) ? keys.privateKeys.active : this.encrypt(keys.privateKeys.active, password, salt).toString()
     },
     publicKeys: { ...publicKeys }
   };
