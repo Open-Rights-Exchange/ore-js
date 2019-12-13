b1837b6e45596dbd21cd08e541f6574bafbc1265
Tray Lewin <traylewin@Trays-iMac.local>
2019-12-12 17:34:44 -0800

    rename enum and members to capital case

--- src/compose.js
+++ src/compose.js
@@ -1,40 +1,40 @@
 /* eslint-disable quote-props */
-import account_deleteAuth from './templates/chainActions/account_deleteAuth';
-import account_linkAuth from './templates/chainActions/account_linkAuth';
-import account_unlinkAuth from './templates/chainActions/account_unlinkAuth';
-import account_updateAuth from './templates/chainActions/account_updateAuth';
-import createBridge_create from './templates/chainActions/createBridge_create';
-import createBridge_define from './templates/chainActions/createBridge_define';
-import createBridge_init from './templates/chainActions/createBridge_init';
-import createBridge_reclaim from './templates/chainActions/createBridge_reclaim';
-import createBridge_transfer from './templates/chainActions/createBridge_transfer';
-import createBridge_whitelist from './templates/chainActions/createBridge_whitelist';
-import ore_upsertRight from './templates/chainActions/ore_upsertRight';
-import token_approve from './templates/chainActions/token_approve';
-import token_create from './templates/chainActions/token_create';
-import token_issue from './templates/chainActions/token_issue';
-import token_retire from './templates/chainActions/token_retire';
-import token_transfer from './templates/chainActions/token_transfer';
-import token_transferFrom from './templates/chainActions/token_transferFrom';
+import Account_DeleteAuth from './templates/chainActions/account_deleteAuth';
+import Account_LinkAuth from './templates/chainActions/account_linkAuth';
+import Account_UnlinkAuth from './templates/chainActions/account_unlinkAuth';
+import Account_UpdateAuth from './templates/chainActions/account_updateAuth';
+import CreateBridge_Create from './templates/chainActions/createBridge_create';
+import CreateBridge_Define from './templates/chainActions/createBridge_define';
+import CreateBridge_Init from './templates/chainActions/createBridge_init';
+import CreateBridge_Reclaim from './templates/chainActions/createBridge_reclaim';
+import CreateBridge_Transfer from './templates/chainActions/createBridge_transfer';
+import CreateBridge_Whitelist from './templates/chainActions/createBridge_whitelist';
+import Ore_UpsertRight from './templates/chainActions/ore_upsertRight';
+import Token_Approve from './templates/chainActions/token_approve';
+import Token_Create from './templates/chainActions/token_create';
+import Token_Issue from './templates/chainActions/token_issue';
+import Token_Retire from './templates/chainActions/token_retire';
+import Token_Transfer from './templates/chainActions/token_transfer';
+import Token_TransferFrom from './templates/chainActions/token_transferFrom';
 
-export const chainAction = {
-  account_deleteAuth,
-  account_linkAuth,
-  account_unlinkAuth,
-  account_updateAuth,
-  createBridge_create,
-  createBridge_define,
-  createBridge_init,
-  createBridge_reclaim,
-  createBridge_transfer,
-  createBridge_whitelist,
-  ore_upsertRight,
-  token_approve,
-  token_create,
-  token_issue,
-  token_retire,
-  token_transfer,
-  token_transferFrom
+export const ChainAction = {
+  Account_DeleteAuth,
+  Account_LinkAuth,
+  Account_UnlinkAuth,
+  Account_UpdateAuth,
+  CreateBridge_Create,
+  CreateBridge_Define,
+  CreateBridge_Init,
+  CreateBridge_Reclaim,
+  CreateBridge_Transfer,
+  CreateBridge_Whitelist,
+  Ore_UpsertRight,
+  Token_Approve,
+  Token_Create,
+  Token_Issue,
+  Token_Retire,
+  Token_Transfer,
+  Token_TransferFrom
 };
 
 export function composeAction(actionType, args) {
