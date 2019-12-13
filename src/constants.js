72ca24e166a16bae4c039cbc613ff0a1f7b38d48
Surabhi Lodha <surabhil@usc.edu>
2019-12-12 17:26:03 -0800

    make code consistent with es5 pattern which is followed everywhere else

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,18 @@
 // sign transaction parameters
-export const TRANSACTION_EXPIRY_IN_SECONDS = 30;
-export const BLOCKS_BEHIND_REF_BLOCK = 3;
-export const TRANSACTION_ENCODING = 'utf8';
+const TRANSACTION_EXPIRY_IN_SECONDS = 30;
+const BLOCKS_BEHIND_REF_BLOCK = 3;
+const TRANSACTION_ENCODING = 'utf8';
 
 // transaction confirmation parameters
-export const BLOCKS_TO_CHECK = 20;
-export const CHECK_INTERVAL = 400;
-export const GET_BLOCK_ATTEMPTS = 5;
+const BLOCKS_TO_CHECK = 20;
+const CHECK_INTERVAL = 400;
+const GET_BLOCK_ATTEMPTS = 5;
+
+module.exports = {
+  TRANSACTION_EXPIRY_IN_SECONDS,
+  BLOCKS_BEHIND_REF_BLOCK,
+  TRANSACTION_ENCODING,
+  BLOCKS_TO_CHECK,
+  CHECK_INTERVAL,
+  GET_BLOCK_ATTEMPTS
+};
